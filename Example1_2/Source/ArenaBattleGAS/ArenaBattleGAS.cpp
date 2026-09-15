bb920d823c7151e069fe199e8fe33aaa63ec81ec
Hyeongjin Kang <73596570+rapidswap@users.noreply.github.com>
2026-09-15 19:19:42 +0900

    Remove Example1_2

--- Example1_2/Source/ArenaBattleGAS/ArenaBattleGAS.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ArenaBattleGAS.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattleGAS, "ArenaBattleGAS" );
